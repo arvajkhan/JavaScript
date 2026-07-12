f2db5f3778f281d93aa42f151ec39e8049ed4443
arvajKhan <arvajkhan194@gmail.com>
2026-07-12 23:41:36 +0530

    Ecommerce

---
+++ Api/One/script.js
@@ -0,0 +1,30 @@
+let result = document.querySelector(".result");
+let show = "";
+
+fetch("https://dummyjson.com/carts")
+.then(res => res.json())
+.then(data => {
+
+    data.carts.forEach(cart => {
+
+        let product = cart.products[0];
+
+        show += `
+        <div class="col-12 col-sm-6 col-lg-3 mb-4">
+            <div class="card h-100">
+                <img src="${product.thumbnail}" class="card-img-top">
+                <div class="card-body">
+                    <h5>Brand: ${product.title}</h5>
+                    <p>Price: ${product.price}</p>
+                    </br>
+                    <button class="btn btn-success px-4 m-4">Buy</button><button class="btn btn-warning px-4">Cart</button>
+                </div>
+            </div>
+        </div>
+        `;
+
+    });
+
+    result.innerHTML = show;
+
+});
