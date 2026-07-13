0f257be1ed60b4fd0bc539d76727ddfe2676e97f
arvajKhan <arvajkhan194@gmail.com>
2026-07-13 18:39:51 +0530

    dd

--- Student/student1/script.js
+++ Student/student1/script.js
@@ -1,36 +1,36 @@
-const searchForm = document.querySelector("#searchForm");
 
-const searchInput = document.querySelector("#searchInput");
-
-
-searchForm.addEventListener("submit", function (event) {
-
-  event.preventDefault();
-
-
-  const searchValue = searchInput.value.trim();
-
-
-  if (searchValue === "") {
-
-    alert("Please enter something to search");
-
-    return;
-
-  }
-
-
-  console.log("Searching:", searchValue);
-
-});
 let result = document.querySelector(".form")
+let search = document.querySelector("#searchInput")
 let show = "";
 let allproduct = [];
 fetch('/JavaScript/Student/student1/mydata.json').
-then((res)=>{
-  return res.json();
-}).then((data)=>{
-  data.users.forEach(element => {
-    
-  });
-})
+  then((res) => {
+    return res.json();
+  }).then((data) => {
+    data.users.forEach(element => {
+      allStudents.push(student);
+      show += `<div class="col-12 col-sm-6 col-lg-4 mb-4">
+                    <div class="card h-100 shadow">
+                        <img src="${student.image}" class="card-img-top" >
+                        <div class="card-body">
+                            <h4> ${student.firstName} ${student.lastName} </h4>
+                            <p>Age: ${student.age}</p>
+                            <p> Email: ${student.email}</p>
+                            <p>Phone: ${student.phone}</p>
+                            <p>Address: ${student.address.city},${student.address.country}</p>
+                            <button class="btn btn-primary">
+                                View Profile
+                            </button>
+                        </div>
+                    </div>
+                </div>
+     `
+    });
+  })
+
+// function btn() {
+//   let output = "";
+//   let filter = allproduct.forEach((element) => {
+//     return element.
+//   })
+// }
