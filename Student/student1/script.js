d5c82ccdad71d7fd27a1605ef0ba0c71610e6254
arvajKhan <arvajkhan194@gmail.com>
2026-07-13 19:29:41 +0530

    gg

--- Student/student1/script.js
+++ Student/student1/script.js
@@ -32,34 +32,77 @@ fetch("https://dummyjson.com/users?utm_source=chatgpt.com").
   }).catch((error) => {
     console.log(error);
   });
+// function btn() {
+//   let searchValue = search.value.toLowerCase();
+//   let output = "";
+
+//   let filter = allStudents.filter((student) => {
+
+//     let fullName = `${student.firstName} ${student.lastName}`.toLowerCase();
+
+//     return (
+//       fullName === searchValue ||
+//       student.phone === search.value ||
+//       student.email.toLowerCase() === searchValue
+//     );
+//   });
+
+//   filter.forEach((student) => {
+//     output += `
+//       <div class="col col-12 col-sm-6 col-lg-3 mb-4">
+//         <div class="card h-100 shadow">
+//           <img src="${student.image}" class="card-img-top">
+
+//           <div class="card-body">
+//             <h4>${student.firstName} ${student.lastName}</h4>
+//             <p>Age: ${student.age}</p>
+//             <p>Email: ${student.email}</p>
+//             <p>Phone: ${student.phone}</p>
+//             <p>Address: ${student.address.city}, ${student.address.country}</p>
+
+//             <button class="btn btn-primary">
+//               View Profile
+//             </button>
+//           </div>
+//         </div>
+//       </div>
+//     `;
+//   });
+
+//   result.innerHTML = output;
+// }
+
+
 function btn() {
-   let searchValue = search.value.toLowerCase();
-  let output = "";
-  let filter = allStudents.filter((element) => {
+  let searchValue = search.value.toLowerCase();
 
+  let student = allStudents.find((student) => {
     let fullName = `${student.firstName} ${student.lastName}`.toLowerCase();
-    return  (fullName === searchValue ||
-      student.phone === search.value ||
-      student.email.toLowerCase() === searchValue)
-  })
-  filter.forEach((student)=>{
-output += `<div class="col col-12 col-sm-6 col-lg-3 mb-4">
-                    <div class="card h-100 shadow">
-                        <img src="${student.image}" class="card-img-top" >
-                        <div class="card-body">
-                            <h4> ${student.firstName} ${student.lastName} </h4>
-                            <p>Age: ${student.age}</p>
-                            <p> Email: ${student.email}</p>
-                            <p>Phone: ${student.phone}</p>
-                            <p>Address: ${student.address.city},${student.address.country}</p>
-                            <button class="btn btn-primary">
-                                View Profile
-                            </button>
-                        </div>
-                    </div>
-                </div>`
 
-  })
- 
-                result.innerHTML = output
+    return (
+      fullName === searchValue ||
+      student.phone === search.value ||
+      student.email.toLowerCase() === searchValue
+    );
+  });
+
+  if (student) {
+    result.innerHTML = `
+      <div class="col col-12 col-sm-6 col-lg-3 mb-4">
+        <div class="card h-100 shadow">
+          <img src="${student.image}" class="card-img-top">
+          <div class="card-body">
+            <h4>${student.firstName} ${student.lastName}</h4>
+            <p>Age: ${student.age}</p>
+            <p>Email: ${student.email}</p>
+            <p>Phone: ${student.phone}</p>
+            <p>Address: ${student.address.city}, ${student.address.country}</p>
+            <button class="btn btn-primary">View Profile</button>
+          </div>
+        </div>
+      </div>
+    `;
+  } else {
+    result.innerHTML = `<h3>Student Not Found</h3>`;
+  }
 }
