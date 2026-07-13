f99acae16fb5db596d97332c4ae0a962924e8150
arvajKhan <arvajkhan194@gmail.com>
2026-07-13 19:14:36 +0530

    aa

--- Student/student1/script.js
+++ Student/student1/script.js
@@ -2,14 +2,15 @@
 let result = document.querySelector(".form")
 let search = document.querySelector("#searchInput")
 let show = "";
-let allproduct = [];
+let allStudents = [];
 fetch("https://dummyjson.com/users?utm_source=chatgpt.com").
   then((res) => {
     return res.json();
   }).then((data) => {
-    data.users.forEach(element => {
+
+    data.users.forEach(student => {
       allStudents.push(student);
-      show += `<div class="col-12 col-sm-6 col-lg-4 mb-4">
+      show += `<div class="col col-12 col-sm-6 col-lg-3 mb-4">
                     <div class="card h-100 shadow">
                         <img src="${student.image}" class="card-img-top" >
                         <div class="card-body">
@@ -28,11 +29,37 @@ fetch("https://dummyjson.com/users?utm_source=chatgpt.com").
 
     });
     result.innerHTML = show;
-  })
+  }).catch((error) => {
+    console.log(error);
+  });
+function btn() {
+   let searchValue = search.value.toLowerCase();
+  let output = "";
+  let filter = allStudents.filter((element) => {
 
-// function btn() {
-//   let output = "";
-//   let filter = allproduct.forEach((element) => {
-//     return element.
-//   })
-// }
+    let fullName = `${student.firstName} ${student.lastName}`.toLowerCase();
+    return  (fullName === searchValue ||
+      student.phone === search.value ||
+      student.email.toLowerCase() === searchValue)
+  })
+  filter.forEach((student)=>{
+output += `<div class="col col-12 col-sm-6 col-lg-3 mb-4">
+                    <div class="card h-100 shadow">
+                        <img src="${student.image}" class="card-img-top" >
+                        <div class="card-body">
+                            <h4> ${student.firstName} ${student.lastName} </h4>
+                            <p>Age: ${student.age}</p>
+                            <p> Email: ${student.email}</p>
+                            <p>Phone: ${student.phone}</p>
+                            <p>Address: ${student.address.city},${student.address.country}</p>
+                            <button class="btn btn-primary">
+                                View Profile
+                            </button>
+                        </div>
+                    </div>
+                </div>`
+
+  })
+ 
+                result.innerHTML = output
+}
