e8edb9afa9734f25b25925ed8e15405fc1a5d8f8
arvajKhan <arvajkhan194@gmail.com>
2026-07-14 08:50:13 +0530

    ss

--- Student/student1/script.js
+++ Student/student1/script.js
@@ -22,6 +22,9 @@ fetch("https://dummyjson.com/users?utm_source=chatgpt.com").
                             <button class="btn btn-primary">
                                 View Profile
                             </button>
+                            <button onclick="deleteStudent(${student.id})">
+  Delete
+</button>
                         </div>
                     </div>
                 </div>
@@ -106,3 +109,53 @@ function btn() {
     result.innerHTML = `<h3>Student Not Found</h3>`;
   }
 }
+
+
+// function btn() {
+//   let searchValue = search.value.toLowerCase();
+
+//   let student = allStudents.find((item) => {
+//     let fullName = `${item.firstName} ${item.lastName}`.toLowerCase();
+
+//     return (
+//       fullName === searchValue ||
+//       item.phone === search.value ||
+//       item.email.toLowerCase() === searchValue
+//     );
+//   });
+
+//   let students = student ? [student] : [];
+
+//   result.innerHTML = students.map((item) => {
+//     return `
+//       <div class="col-12 col-sm-6 col-lg-3 mb-4">
+//         <div class="card h-100 shadow">
+
+//           <img src="${item.image}" class="card-img-top">
+
+//           <div class="card-body">
+
+//             <h4>${item.firstName} ${item.lastName}</h4>
+
+//             <p>Age: ${item.age}</p>
+
+//             <p>Email: ${item.email}</p>
+
+//             <p>Phone: ${item.phone}</p>
+
+//             <p>Address: ${item.address.city}, ${item.address.country}</p>
+
+//             <button class="btn btn-primary">
+//               View Profile
+//             </button>
+
+//           </div>
+//         </div>
+//       </div>
+//     `;
+//   }).join("");
+
+//   if (students.length === 0) {
+//     result.innerHTML = `<h3>Student Not Found</h3>`;
+//   }
+// }
