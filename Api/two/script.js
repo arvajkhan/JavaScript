27f61a14c31c2299cdb2c9c883a55e572e939c12
arvajKhan <arvajkhan194@gmail.com>
2026-07-15 09:38:15 +0530

    ff

--- Api/two/script.js
+++ Api/two/script.js
@@ -4,20 +4,20 @@ let allStudents = [];
 
 // Fetch API
 fetch("https://dummyjson.com/users")
-  .then(res => res.json())
-  .then(data => {
+.then(res => res.json())
+.then(data=>{
     allStudents = data.users;
     display(allStudents);
-  });
+});
 
 // Display Function
-function display(data) {
+function display(data){
 
-  result.innerHTML = "";
+    result.innerHTML = "";
 
-  data.forEach(student => {
+    data.forEach(student=>{
 
-    result.innerHTML += `
+        result.innerHTML += `
         <div class="card">
             <img src="${student.image}">
             <h3>${student.firstName} ${student.lastName}</h3>
@@ -29,37 +29,36 @@ function display(data) {
         </div>
         `;
 
-  });
+    });
 
 }
 
 
-
 // Search
-function searchStudent() {
+function searchStudent(){
 
-  let value = document
+    let value=document
     .getElementById("search")
     .value
     .toLowerCase();
 
-  let filter = allStudents.filter(student =>
+    let filter=allStudents.filter(student=>
 
-    student.firstName.toLowerCase().includes(value) ||
+        student.firstName.toLowerCase().includes(value) ||
 
-    student.lastName.toLowerCase().includes(value)
+        student.lastName.toLowerCase().includes(value)
 
-  );
+    );
 
-  display(filter);
+    display(filter);
 
 }
 
 // Delete
-function deleteStudent(id) {
+function deleteStudent(id){
 
-  allStudents = allStudents.filter(student => student.id !== id);
+    allStudents=allStudents.filter(student=>student.id!==id);
 
-  display(allStudents);
+    display(allStudents);
 
 }
