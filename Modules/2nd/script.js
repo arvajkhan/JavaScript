ea22d8a807e395876ca705df6008080963effcf1
arvajKhan <arvajkhan194@gmail.com>
2026-07-27 00:21:28 +0530

    fk

--- Modules/2nd/script.js
+++ Modules/2nd/script.js
@@ -1,19 +1,26 @@
 import { arr } from "./arr.js";
 
-let result = document.querySelector(".result")
-let show = ""
-    arr.forEach((student) => {
+let result = document.querySelector(".result");
+
+export function display(data) {
+
+    let show = "";
+
+    data.forEach((student) => {
         show += `
         <div class="col-12 col-sm-6 col-md-4 col-lg-2 card m-2">
-                <img src="${student.image}" width="100">
-                <h3>${student.name}</h3>
-                <p>Roll No : ${student.rollNumber}</p>
-                <p>Age : ${student.age}</p>
-                <p>Course : ${student.course}</p>
-                <p>Branch : ${student.branch}</p>
-                <hr>
-            </div>
+            <img src="${student.image}" width="100">
+            <h3>${student.name}</h3>
+            <p>Roll No : ${student.rollNumber}</p>
+            <p>Age : ${student.age}</p>
+            <p>Course : ${student.course}</p>
+            <p>Branch : ${student.branch}</p>
+            <hr>
+        </div>
         `;
     });
 
     result.innerHTML = show;
+}
+
+display(arr);
--- Modules/2nd/search.js
+++ Modules/2nd/search.js
@@ -1,5 +1,18 @@
 import { arr } from "./arr.js";
-let search = document.querySelector(".search")
-function btn(){
-  
+import { display } from "./script.js";
+
+let search = document.querySelector(".search");
+
+window.btn = function () {
+
+    let value = search.value.toLowerCase();
+
+    let filterData = arr.filter(student =>
+        student.name.toLowerCase().includes(value) ||
+        student.rollNumber.toString().includes(value) ||
+        student.course.toLowerCase().includes(value) ||
+        student.branch.toLowerCase().includes(value)
+    );
+
+    display(filterData);
 }
